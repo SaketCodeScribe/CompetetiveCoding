9b61682a540841663e8953adb87fde616cd5e2dd
SaketCodeScribe <saket0565@gmail.coom>
2024-02-27 21:46:43 +0530

    topic: Binary Search Tree, Recursion

---
+++ CompetetiveCoding/src/com/dsa_algorithms/BinarySearchTree/ConstructBSTfromPreorderTraversal.java
@@ -0,0 +1,29 @@
+package com.dsa_algorithms.BinarySearchTree;
+
+public class ConstructBSTfromPreorderTraversal {
+    int n;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        n = preorder.length;
+        return bstFromPreOrder(0, n-1, preorder);
+    }
+    public TreeNode bstFromPreOrder(int l, int r, int[] preorder){
+        if (l > r)
+            return null;
+        if (l == r)
+            return new TreeNode(preorder[l]);
+        TreeNode root = new TreeNode(preorder[l]);
+        int i = l;
+        for(i=l+1; i<=r; i++){
+            if (preorder[l] < preorder[i]){
+                break;
+            }
+        }
+        if (i > r)
+            root.left = bstFromPreOrder(l+1, r, preorder);
+        else{
+            root.left = bstFromPreOrder(l+1, i-1, preorder);
+            root.right = bstFromPreOrder(i, r, preorder);
+        }
+        return root;
+    }
+}
