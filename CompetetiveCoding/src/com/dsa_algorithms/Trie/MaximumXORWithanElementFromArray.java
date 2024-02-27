b9b198a2953b485b354e73502720f91c5755563d
SaketCodeScribe <saket0565@gmail.coom>
2024-02-27 21:04:15 +0530

    topic: trie, bit-manipulation

--- CompetetiveCoding/src/com/dsa_algorithms/Trie/MaximumXORWithanElementFromArray.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Trie/MaximumXORWithanElementFromArray.java
@@ -8,37 +8,37 @@ public int compare(int[] a, int[] b){
     return a[1]-b[1];
 }
 }
-class Node {
-    Node links[] = new Node[2];
-    public Node() {}
+class TrieNodes {
+    TrieNodes links[] = new TrieNodes[2];
+    public TrieNodes() {}
     boolean containsKey(int ind) {
         return (links[ind] != null);
     }
-    Node get(int ind) {
+    TrieNodes get(int ind) {
         return links[ind];
     }
-    void put(int ind, Node node) {
+    void put(int ind, TrieNodes node) {
         links[ind] = node;
     }
 }
 class Trie {
-    private static Node root;
+    private static TrieNodes root;
     Trie() {
-        root = new Node();
+        root = new TrieNodes();
     }
     public static void insert(int num) {
-        Node node = root;
+        TrieNodes node = root;
         for(int i = 31;i>=0;i--) {
             int bit = (num >> i) & 1;
             if(!node.containsKey(bit)) {
-                node.put(bit, new Node());
+                node.put(bit, new TrieNodes());
             }
             node = node.get(bit);
         }
     }
 
     public int getMax(int num) {
-        Node node = root;
+        TrieNodes node = root;
         int maxNum = 0;
         for(int i = 31;i>=0 && node != null;i--) {
             int bit = (num >> i) & 1;
