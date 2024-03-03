cd995201d0321445ff1b4d88166702e65c0b055e
SaketCodeScribe <saket0565@gmail.coom>
2024-03-03 22:07:06 +0530

    topic: Tree, DFS

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Tree/LCA.java
@@ -0,0 +1,21 @@
+package com.dsa_algorithms.Tree;
+
+public class LCA {
+    TreeNode lca;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        lca = null;
+        findLca(root, p, q);
+        return lca;
+    }
+    public boolean findLca(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null)
+            return false;
+        boolean left = findLca(root.left, p, q);
+        boolean right = findLca(root.right, p, q);
+        if (((root.val == p.val || root.val == q.val) && (left || right)) || (left && right)){
+            lca = root;
+            return true;
+        }
+        return root.val == p.val || root.val == q.val;
+    }
+}
