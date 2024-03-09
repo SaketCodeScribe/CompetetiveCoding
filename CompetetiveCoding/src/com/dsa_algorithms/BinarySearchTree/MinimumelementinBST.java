fafffa5afcd036bf62ad0c2b337b01073fffab36
SaketCodeScribe <saket0565@gmail.coom>
2024-03-09 13:00:35 +0530

    topic: Revert changes.

--- CompetetiveCoding/src/com/dsa_algorithms/BinarySearchTree/MinimumelementinBST.java
+++ CompetetiveCoding/src/com/dsa_algorithms/BinarySearchTree/MinimumelementinBST.java
@@ -1,12 +1,12 @@
-//package com.dsa_algorithms.BinarySearchTree;
-// Commenting to ignore build fail
-//public class MinimumelementinBST {
-//    public static int minValue(Node root) {
-//        if (root == null)
-//            return -1;
-//        // Write your code here.
-//        while(root.left != null)
-//            root = root.left;
-//        return root.data;
-//    }
-//}
+package com.dsa_algorithms.BinarySearchTree;
+ Commenting to ignore build fail
+public class MinimumelementinBST {
+    public static int minValue(Node root) {
+        if (root == null)
+            return -1;
+        // Write your code here.
+        while(root.left != null)
+            root = root.left;
+        return root.data;
+    }
+}
--- CompetetiveCoding/src/com/dsa_algorithms/Tree/ChildrenSumProperty.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Tree/ChildrenSumProperty.java
@@ -1,12 +1,12 @@
 package com.dsa_algorithms.Tree;
 
 public class ChildrenSumProperty {
-//    public static boolean isParentSum(Node root) {
-//        // Write your code here.
-//        if (root == null)
-//            return true;
-//        if (root.left == null && root.right == null)
-//            return true;
-//        return isParentSum(root.left) && isParentSum(root.right) && (root.data == (root.left != null ? root.left.data : 0)+(root.right != null ? root.right.data : 0));
-//    }
+    public static boolean isParentSum(Node root) {
+        // Write your code here.
+        if (root == null)
+            return true;
+        if (root.left == null && root.right == null)
+            return true;
+        return isParentSum(root.left) && isParentSum(root.right) && (root.data == (root.left != null ? root.left.data : 0)+(root.right != null ? root.right.data : 0));
+    }
 }
