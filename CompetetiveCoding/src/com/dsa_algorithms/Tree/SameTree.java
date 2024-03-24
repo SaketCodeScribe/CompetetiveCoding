227d235441a1d7f21fa75561a75d9eebba228b4a
SaketCodeScribe <saket0565@gmail.coom>
2024-03-24 23:16:50 +0530

    topic: Tree, Traversal

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Tree/SameTree.java
@@ -0,0 +1,11 @@
+package com.dsa_algorithms.Tree;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if ((p == null && q != null) || (p != null && q == null))
+            return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
