484bb5f81efce67061281f51f217dae9037d9150
SaketCodeScribe <saket0565@gmail.coom>
2024-03-24 22:56:54 +0530

    topic: Tree, Traversal

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Tree/isHeightBalancedTree.java
@@ -0,0 +1,31 @@
+package com.dsa_algorithms.Tree;
+
+class TreeNode<T>
+{
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+public class isHeightBalancedTree {
+    public static boolean isBalancedBT(TreeNode<Integer> root) {
+        return isBalanced(root) != -1 ? true : false;
+    }
+
+    public static int isBalanced(TreeNode<Integer> root){
+        if (root == null)
+            return 0;
+        int left = isBalanced(root.left);
+        if (left == -1)
+            return -1;
+        int right = isBalanced(root.right);
+        if (right == -1 || Math.abs(left-right) > 1)
+            return -1;
+        return Math.max(left, right)+1;
+    }
+}
