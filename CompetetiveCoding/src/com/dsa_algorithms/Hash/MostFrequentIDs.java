6dcd7bd1a6bf939f0e4c223a1d931c3c081126ed
SaketCodeScribe <saket0565@gmail.coom>
2024-03-26 00:28:59 +0530

    topic: Priority Queue, Map

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Hash/MostFrequentIDs.java
@@ -0,0 +1,28 @@
+package com.dsa_algorithms.Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class MostFrequentIDs {
+    public long[] mostFrequentIDs(int[] nums, int[] freq) {
+        PriorityQueue<long[]> pq = new PriorityQueue<long[]>((a, b) -> (int) (b[1] - a[1]));
+        int i = 0, n = nums.length, j = 0;
+        long[] ans = new long[n];
+        Map<Integer, Long> map = new HashMap<>();
+
+        while (i < n) {
+            map.put(nums[i], map.getOrDefault(nums[i], new Long(0)) + freq[i]);
+            while (!pq.isEmpty()) {
+                long[] num = pq.peek();
+                if (map.get((int) num[0]) != num[1])
+                    pq.poll();
+                else
+                    break;
+            }
+            pq.add(new long[]{nums[i], map.get(nums[i++])});
+            ans[j++] = pq.peek()[1];
+        }
+        return ans;
+    }
+}
