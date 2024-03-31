6d96cf72b87093884f33224990152d4b3279236a
SaketCodeScribe <saket0565@gmail.coom>
2024-03-31 23:13:27 +0530

    topic: Pattern Matching, KMP

---
+++ CompetetiveCoding/src/com/dsa_algorithms/PatternMatching.java
@@ -0,0 +1,37 @@
+package com.dsa_algorithms;
+
+import java.util.*;
+
+public class PatternMatching {
+    public int strStr(String haystack, String needle) {
+        int i, j, m = haystack.length(), n = needle.length();
+        int[] lps = new int[n];
+        Arrays.fill(lps, -1);
+        j = -1;
+        i = 1;
+        while(i<n && j<i){
+            if (needle.charAt(j+1) == needle.charAt(i))
+                lps[i++] = ++j;
+            else{
+                if (j == -1)
+                    i++;
+                else
+                    j = lps[j];
+            }
+        }
+        i = j = 0;
+        while(i<m && j<n){
+            if (haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                if (j < 1)
+                    i++;
+                else
+                    j = lps[j-1]+1;
+            }
+        }
+        return j >= n ? i-n : -1;
+    }
+}
