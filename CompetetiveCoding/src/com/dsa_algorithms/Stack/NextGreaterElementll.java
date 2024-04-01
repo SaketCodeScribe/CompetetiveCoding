84b224c20df773aa401d1076d15d7c4c346e28b3
SaketCodeScribe <saket0565@gmail.coom>
2024-04-01 16:32:21 +0530

    topic: Prefix sum

--- CompetetiveCoding/src/com/dsa_algorithms/Stack/NextGreaterElementll.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Stack/NextGreaterElementll.java
@@ -9,20 +9,20 @@ public class NextGreaterElementll {
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
         Stack<Integer> stack = new Stack<>();
-        for(i=1; i<n; i++)
+        for(i=0; i<n; i++)
             stack.add(i);
         i = 0;
         while (i<n){
-            while(!stack.empty() && a[stack.peek()] < a[i%n]){
+            while(!stack.empty() && a[stack.peek()] < a[i]){
                 int ind = stack.pop();
-                ans[ind] = a[i%n];
+                ans[ind] = a[i];
             }
-            stack.push((i++)%n);
+            stack.push(i++);
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        Arrays.stream(nextGreaterElementII(new int[]{1,5,3,4,2})).forEach(value -> System.out.println(value));
+        Arrays.stream(nextGreaterElementII(new int[]{1,5,3,4,2})).forEach(value -> System.out.print(value+" "));
     }
 }
