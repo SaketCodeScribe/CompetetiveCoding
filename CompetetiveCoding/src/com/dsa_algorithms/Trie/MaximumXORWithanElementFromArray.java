99842924980d2a9e45d78c2df693f179378150bf
SaketCodeScribe <saket0565@gmail.coom>
2024-04-07 12:03:49 +0530

    topic: Adds static class to make Node class private

--- CompetetiveCoding/src/com/dsa_algorithms/Trie/MaximumXORWithanElementFromArray.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Trie/MaximumXORWithanElementFromArray.java
@@ -8,53 +8,54 @@ public int compare(int[] a, int[] b){
     return a[1]-b[1];
 }
 }
-class TrieNodes {
-    TrieNodes links[] = new TrieNodes[2];
-    public TrieNodes() {}
-    boolean containsKey(int ind) {
-        return (links[ind] != null);
-    }
-    TrieNodes get(int ind) {
-        return links[ind];
-    }
-    void put(int ind, TrieNodes node) {
-        links[ind] = node;
-    }
-}
-class Trie {
-    private static TrieNodes root;
-    Trie() {
-        root = new TrieNodes();
-    }
-    public static void insert(int num) {
-        TrieNodes node = root;
-        for(int i = 31;i>=0;i--) {
-            int bit = (num >> i) & 1;
-            if(!node.containsKey(bit)) {
-                node.put(bit, new TrieNodes());
-            }
-            node = node.get(bit);
+
+public class MaximumXORWithanElementFromArray {
+    static class TrieNodes {
+        TrieNodes links[] = new TrieNodes[2];
+        public TrieNodes() {}
+        boolean containsKey(int ind) {
+            return (links[ind] != null);
+        }
+        TrieNodes get(int ind) {
+            return links[ind];
+        }
+        void put(int ind, TrieNodes node) {
+            links[ind] = node;
         }
     }
 
-    public int getMax(int num) {
-        TrieNodes node = root;
-        int maxNum = 0;
-        for(int i = 31;i>=0 && node != null;i--) {
-            int bit = (num >> i) & 1;
-            if(node.containsKey(1 - bit)) {
-                maxNum = maxNum | (1<<i);
-                node = node.get( 1 - bit);
-            }
-            else {
+    static class Trie {
+        private static TrieNodes root;
+        Trie() {
+            root = new TrieNodes();
+        }
+        public static void insert(int num) {
+            TrieNodes node = root;
+            for(int i = 31;i>=0;i--) {
+                int bit = (num >> i) & 1;
+                if(!node.containsKey(bit)) {
+                    node.put(bit, new TrieNodes());
+                }
                 node = node.get(bit);
             }
         }
-        return maxNum;
-    }
-}
 
-public class MaximumXORWithanElementFromArray {
+        public int getMax(int num) {
+            TrieNodes node = root;
+            int maxNum = 0;
+            for(int i = 31;i>=0 && node != null;i--) {
+                int bit = (num >> i) & 1;
+                if(node.containsKey(1 - bit)) {
+                    maxNum = maxNum | (1<<i);
+                    node = node.get( 1 - bit);
+                }
+                else {
+                    node = node.get(bit);
+                }
+            }
+            return maxNum;
+        }
+    }
     private Trie trie;
     public int[] maximizeXor(int[] nums, int[][] queries) {
         int i = 0;
