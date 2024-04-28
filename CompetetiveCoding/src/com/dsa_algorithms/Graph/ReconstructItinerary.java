e768483e9d0a9716bda5e98b109173b17be61413
SaketCodeScribe <saket0565@gmail.coom>
2024-04-28 15:45:27 +0530

    topic: Graph, Backtracking, DFS

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Graph/ReconstructItinerary.java
@@ -0,0 +1,41 @@
+package com.dsa_algorithms.Graph;
+
+import java.util.*;
+public class ReconstructItinerary {
+    List<String> ans;
+    Map<String, TreeMap<String, Integer>> map;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        ans = new ArrayList<>();
+        map = new HashMap<>();
+        if (tickets == null || tickets.size() == 0)
+            return ans;
+        for(List<String> ticket:tickets){
+            String dep = ticket.get(0), arr = ticket.get(1);
+            if (map.get(dep) == null)
+                map.put(dep, new TreeMap<>());
+            TreeMap<String, Integer> tree = map.get(dep);
+            tree.put(arr, tree.getOrDefault(arr,0)+1);
+        }
+        dfs("JFK", tickets.size());
+        return ans;
+    }
+    public boolean dfs(String node, int tot){
+        ans.add(node);
+        if (tot == 0)
+            return true;
+        if (map.get(node) == null)
+            return false;
+        for(Map.Entry<String, Integer> entry:map.get(node).entrySet()){
+            String child = entry.getKey();
+            int cnt = entry.getValue();
+            if (cnt == 0)
+                continue;
+            entry.setValue(cnt-1);
+            if (dfs(child, tot-1))
+                return true;
+            ans.remove(ans.size()-1);
+            entry.setValue(entry.getValue()+1);
+        }
+        return false;
+    }
+}
