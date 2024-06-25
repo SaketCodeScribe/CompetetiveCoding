1127c4df98b847cff5ee9f180e40385c194f363c
SaketCodeScribe <saket0565@gmail.coom>
2024-06-25 23:35:22 +0530

    topic: DLL, Map

---
+++ CompetetiveCoding/src/com/dsa_algorithms/LinkedList/LRUCache.java
@@ -0,0 +1,72 @@
+package com.dsa_algorithms.LinkedList;
+
+import java.util.*;
+
+public class LRUCache {
+    static class Node{
+        int data;
+        Node left, right;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+    Node head, tail;
+    Map<Integer, Node> map;
+    int size, capacity;
+
+    public LRUCache(int sizeOfCache) {
+        this.capacity = sizeOfCache;
+        map = new HashMap<>();
+        head = tail = null;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        Node node = map.get(key);
+        deleteNode(node);
+        addToRear(node);
+        return node.data;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)){
+            Node node = map.get(key);
+            node.data = value;
+            deleteNode(node);
+            addToRear(node);
+        }
+        else{
+            Node node = new Node(value);
+            if (size == capacity){
+                map.remove(head.data);
+                deleteNode(head);
+            }
+            else
+                size++;
+            addToRear(node);
+            map.put(key, node);
+        }
+    }
+    public void deleteNode(Node node){
+        Node left = node.left, right = node.right;
+        node.left = node.right = null;
+        if (left != null)
+            left.right = right;
+        else
+            head = right;
+        if (right != null)
+            right.left = left;
+        else
+            tail = left;
+    }
+    public void addToRear(Node node){
+        if (head == null)
+            head = tail = node;
+        else{
+            tail.right = node;
+            node.left = tail;
+            tail = node;
+        }
+    }
+}
