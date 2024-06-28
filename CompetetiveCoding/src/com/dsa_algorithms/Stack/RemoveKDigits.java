3ae1e9a3c245d39cb7fac69073b38d644cdee954
SaketCodeScribe <saket0565@gmail.com>
2024-06-28 13:58:08 +0530

    topic: Stack/Deque

--- CompetetiveCoding/src/com/dsa_algorithms/Stack/RemoveKDigits.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Stack/RemoveKDigits.java
@@ -1,34 +1,47 @@
 package com.dsa_algorithms.Stack;
 
-import java.util.Objects;
-import java.util.Stack;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class RemoveKDigits {
-    public String removeKdigits(String num, int k) {
-        Stack<Integer> stack = new Stack<>();
-        int i, j = 0, n = num.length();
-        if (k == n)
+    Deque<Character> deque;
+    int k;
+    public String removeKdigits(String num, int K) {
+        int n = num.length();
+        k = K;
+        if (n == k)
             return "0";
-        while(j < n && k > 0){
-            int val = num.charAt(j)-'0';
-            while (k > 0 && !stack.isEmpty() && stack.peek() > val){
-                stack.pop();
+
+        num = removeDigits(num, 0, n-1);
+        num = removeZeroesFromBeg(num);
+        return k > num.length() ? "0" : num.substring(0, num.length()-k);
+    }
+
+    public String removeZeroesFromBeg(String num){
+        int i = 0;
+        while(i < num.length()){
+            if (num.charAt(i) != '0')
+                break;
+            i++;
+        }
+        return i >= num.length() ? "0" : num.substring(i, num.length());
+    }
+
+    public String removeDigits(String num, int s, int e){
+        deque = new LinkedList();
+        StringBuilder sb = new StringBuilder();
+        int i = s;
+
+        while(i <= e && k > 0){
+            while(!deque.isEmpty() && k > 0 && deque.peekLast() > num.charAt(i)){
+                deque.pollLast();
                 k--;
             }
-            if (!(stack.isEmpty() && val == 0))
-                stack.push(val);
-            j++;
+            deque.addLast(num.charAt(i++));
         }
-        while (k-- > 0 && !stack.isEmpty())
-            stack.pop();
-        i = j;
-        while(i < n && num.charAt(i) == '0')
-            i++;
-        if (i == n && stack.isEmpty())
-            return "0";
-        if (stack.isEmpty())
-            return num.substring(i);
-        return stack.stream().map(String::valueOf).collect(Collectors.joining(""))+num.substring(j);
+        while(!deque.isEmpty())
+            sb.append(deque.pollFirst());
+        while(i <= e)
+            sb.append(num.charAt(i++));
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
