99b8f7497754b687025a981f66e306d63d1a3c16
SaketCodeScribe <saket0565@gmail.com>
2024-07-03 18:41:24 +0530

    topic: Priority Queue

---
+++ CompetetiveCoding/src/com/dsa_algorithms/PriorityQueue/KthLargestElement.java
@@ -0,0 +1,18 @@
+package com.dsa_algorithms.PriorityQueue;
+
+import java.util.*;
+public class KthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int num : nums){
+            if (pq.size() < k)
+                pq.add(num);
+            else if (num > pq.peek()){
+                pq.poll();
+                pq.add(num);
+            }
+        }
+        return pq.peek();
+    }
+}
