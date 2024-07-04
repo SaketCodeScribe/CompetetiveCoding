a85ad19eaf1fc803bf94bb43ef22a8df83e34439
SaketCodeScribe <saket0565@gmail.com>
2024-07-04 19:43:31 +0530

    topic: Hashing

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Hash/FirstUniqueCharacterinAString.java
@@ -0,0 +1,16 @@
+package com.dsa_algorithms.Hash;
+
+public class FirstUniqueCharacterinAString {
+    public int firstUniqChar(String s) {
+        int[] chars = new int[26];
+
+        for(char ch:s.toCharArray())
+            chars[ch-'a']++;
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (chars[ch-'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
