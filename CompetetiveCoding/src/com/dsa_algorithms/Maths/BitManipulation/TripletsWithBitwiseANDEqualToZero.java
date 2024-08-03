06f02dd70603ed19d01757fb920588064886c9d1
SaketCodeScribe <saket0565@gmail.com>
2024-08-03 22:56:27 +0530

    Commit: Maths, Bit, HashSet

--- CompetetiveCoding/src/com/dsa_algorithms/Maths/BitManipulation/TripletsWithBitwiseANDEqualToZero.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Maths/BitManipulation/TripletsWithBitwiseANDEqualToZero.java
@@ -2,15 +2,19 @@ package com.dsa_algorithms.Maths.BitManipulation;
 
 public class TripletsWithBitwiseANDEqualToZero {
     public int countTriplets(int[] nums) {
-        int i, ans = 0;
-        int[] cnt = new int[1<<16];
+        int i, ans = 0, max = 0, n = 1;
+        for(int num:nums)
+            max = Math.max(max, num);
+        while(n < max)
+            n = n<<1;
+        int[] cnt = new int[n+1];
 
         for(int a:nums){
             for(int b:nums)
                 cnt[a&b]++;
         }
         for(int a:nums){
-            for(i=0; i<(1<<16); i++)
+            for(i=0; i<=n; i++)
                 if((a&i) == 0)
                     ans += cnt[i];
         }
