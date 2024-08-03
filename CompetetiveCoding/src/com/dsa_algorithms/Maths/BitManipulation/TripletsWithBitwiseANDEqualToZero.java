1a2aee8bb74e333ec64922e2b8b31cc752982aab
SaketCodeScribe <saket0565@gmail.com>
2024-08-03 23:01:27 +0530

    Commit: Maths, Bit, Dp and HashSet

--- CompetetiveCoding/src/com/dsa_algorithms/Maths/BitManipulation/TripletsWithBitwiseANDEqualToZero.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Maths/BitManipulation/TripletsWithBitwiseANDEqualToZero.java
@@ -20,4 +20,18 @@ public class TripletsWithBitwiseANDEqualToZero {
         }
         return ans;
     }
+    // Dp
+    public int countTriplets1(int[] A) {
+        int N = 1 << 16, M = 3;
+        int[][] dp = new int[M + 1][N];
+        dp[0][N - 1] = 1;
+        for (int i = 0; i < M; i++) {
+            for (int k = 0; k < N; k++) {
+                for (int a : A) {
+                    dp[i + 1][k & a] += dp[i][k];
+                }
+            }
+        }
+        return dp[M][0];
+    }
 }
