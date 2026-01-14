729042efc313dbf94d91010fe034b84710f402f4
SaketCodeScribe <saket0565@gmail.com>
2026-01-14 07:17:20 +0530

    Code commit

---
+++ CompetetiveCoding/src/com/dsa_algorithms/DynamicProgramming/OnTrees/LC337.java
@@ -0,0 +1,31 @@
+package com.dsa_algorithms.DynamicProgramming.OnTrees;
+
+public class LC337 {
+    static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public int rob(TreeNode root) {
+        int[] robbery = solve(root);
+        return Math.max(robbery[0], robbery[1]);
+    }
+    private int[] solve(TreeNode node){
+        if (node == null){
+            return new int[2];
+        }
+        int[] left = solve(node.left);
+        int leftChildInc = left[0], leftChildExc = left[1];
+        int[] right = solve(node.right);
+        int rightChildInc = right[0], rightChildExc = right[1];
+
+        return new int[]{node.val + leftChildExc + rightChildExc, Math.max(leftChildInc, leftChildExc) + Math.max(rightChildInc, rightChildExc)};
+    }
+}
