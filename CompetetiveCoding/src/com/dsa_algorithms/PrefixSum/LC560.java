576d8cf5725e690d3145a3c816e1ab32762ac096
SaketCodeScribe <saket0565@gmail.com>
2026-07-02 18:41:15 +0530

    add code

---
+++ CompetetiveCoding/src/com/dsa_algorithms/PrefixSum/LC560.java
@@ -0,0 +1,20 @@
+package com.dsa_algorithms.PrefixSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC560 {
+    public int subArraySum(int[] nums, int k) {
+        int i, n = nums.length, ans = 0;
+        long sum = 0;
+        Map<Long, Integer> count = new HashMap<>();
+        count.put(0l, 1);
+
+        for(int num:nums){
+            sum += num;
+            ans += count.getOrDefault(sum - k, 0);
+            count.compute(sum, (key,value) -> value == null ? 1 : value+1);
+        }
+        return ans;
+    }
+}
