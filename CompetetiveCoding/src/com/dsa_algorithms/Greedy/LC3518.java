a773fc6f191a029ca93eea03d4154671fb9d566d
SaketCodeScribe <saket0565@gmail.com>
2026-08-01 21:56:23 +0530

    add code

---
+++ CompetetiveCoding/src/com/dsa_algorithms/Greedy/LC3518.java
@@ -0,0 +1,72 @@
+package com.dsa_algorithms.Greedy;
+
+public class LC3518 {
+    private final long CAP = (long)(1e7);
+    public String smallestPalindrome(String s, int K) {
+        int i = 0, halfLen;
+        long k = K;
+        int[] freq = new int[26];
+        StringBuilder sb = new StringBuilder();
+        updateFrequencyMap(s, freq);
+        char mid = halfFreqAndGetOddCharacter(freq);
+        halfLen = getHalfLength(freq);
+
+        if (multinomial(freq, halfLen) < k) return "";
+
+        for(int pos = 0; pos < halfLen; pos++){
+            i = 0;
+            while(i < 26) {
+                if (freq[i] > 0) {
+                    freq[i]--;
+                    long ways = multinomial(freq, halfLen - pos - 1);
+
+                    if (k <= ways) {
+                        sb.append((char)(i+'a'));
+                        break;
+                    } else {
+                        k -= ways;
+                        freq[i]++;
+                    }
+                }
+                i++;
+            }
+        }
+        if (k > 1) return "";
+        String first = sb.toString();
+        String second = sb.reverse().toString();
+        return first + (mid != 0 ? mid : "") + second;
+    }
+    private int getHalfLength(int[] freq) {
+        int cnt = 0;
+        for(int f:freq) {
+            cnt += f;
+        }
+        return cnt;
+    }
+    private long multinomial(int[] freq, int n) {
+        long res = 1;
+        int used = 0;
+
+        for(int c=0; c<26; c++) {
+            for(int f=1; f<=freq[c]; f++) {
+                used++;
+                res = res * used / f;
+                if (res > CAP) return CAP;
+            }
+        }
+        return res;
+    }
+    private void updateFrequencyMap(String s, int[] freq) {
+        for(char ch:s.toCharArray()) {
+            freq[ch-'a']++;
+        }
+    }
+    private char halfFreqAndGetOddCharacter(int[] freq) {
+        char mid = 0;
+        for(int i=0; i<26; i++) {
+            if (freq[i]%2 != 0) mid = (char)(i + 'a');
+            freq[i] /= 2;
+        }
+        return mid;
+    }
+}
