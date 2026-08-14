63eaf9b3b03ed54f9384a7e7d08140e611ffbce5
SaketCodeScribe <saket0565@gmail.com>
2026-08-15 00:10:30 +0530

    add code

---
+++ CompetetiveCoding/src/com/dsa_algorithms/BackTracking/LC71.java
@@ -0,0 +1,33 @@
+package com.dsa_algorithms.BackTracking;
+
+public class LC71 {
+    private static final int[][] DIRS = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                if (word.charAt(0) == board[i][j] && dfs(board, word, m, n, 0, i, j)) return true;
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int m, int n, int ind, int i, int j) {
+        char ch = board[i][j];
+        board[i][j] = 0;
+
+        if (ind == word.length() - 1) {
+            return true;
+        }
+
+        for (int[] dir : DIRS) {
+            int x = i + dir[0], y = j + dir[1];
+            if (x >= 0 && y >= 0 && x < m && y < n && board[x][y] == word.charAt(ind + 1)) {
+                if (dfs(board, word, m, n, ind + 1, x, y)) return true;
+            }
+        }
+        board[i][j] = ch;
+        return false;
+    }
+}
