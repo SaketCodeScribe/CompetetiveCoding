7437d69f1a689b3f06525cc593f0080f334afca4
SaketCodeScribe <saket0565@gmail.com>
2026-08-26 19:10:33 +0530

    add code

--- CompetetiveCoding/src/com/dsa_algorithms/Stack/LC503.java
+++ CompetetiveCoding/src/com/dsa_algorithms/Stack/LC503.java
@@ -5,18 +5,18 @@ import java.util.Stack;
 
 public class LC503 {
     public int[] nextGreaterElements(int[] nums) {
-        int i, n = nums.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans, -1);
-        Stack<Integer> stack = new Stack<>();
+        int n = nums.length, i, j = -1;
+        int[] stack = new int[n];
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
 
-        for(i=0; i<2*n-1; i++){
-            int id = i%n;
-            while(!stack.isEmpty() && nums[stack.peek()] < nums[id]){
-                ans[stack.pop()] = nums[id];
+
+        for(i=0; i<2*n; i++) {
+            while(j >= 0 && nums[stack[j%n]] < nums[i%n]) {
+                result[stack[(j--)%n]] = nums[i%n];
             }
-            stack.push(id);
+            stack[(++j)%n] = i%n;
         }
-        return ans;
+        return result;
     }
 }
